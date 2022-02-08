f687acd8c272e5c2e1ebe5dd6b4ff143532574c0
shanaka11 <shanakaabeysinghe@gmail.com>
2022-02-08 16:14:05 +0530

    Added Education Page

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,9 +1,11 @@
 import Title from '../components/common/Title';
+import EducationHistory from '../components/education/EducationHistory';
 import { Page } from '../components/styled/Page.styled';
 
 const EducationPage = () => {
     return  <Page>
                 <Title title='Education'/>
+                <EducationHistory />
             </Page>;
 };
 
