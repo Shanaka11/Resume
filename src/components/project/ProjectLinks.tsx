7801202685689cf67a2982941e1ba9dca3724d07
shanaka11 <shanakaabeysinghe@gmail.com>
2022-02-17 12:03:11 +0530

    Code Cleanup

--- src/components/project/ProjectLinks.tsx
+++ src/components/project/ProjectLinks.tsx
@@ -3,7 +3,6 @@ import { IOtherLink, IProjectLinks } from "../../interfaces/IProject"
 import { StyledProjectLinks } from "../styled/Project.styled"
 import ProjectLink from "./ProjectLink"
 import { Link } from 'react-router-dom'
-import { StyledLink } from "../styled/Navigator.styled"
 
 export const ProjectLinks:React.FC<IProjectLinks> = ({ links }) => {
   return (
